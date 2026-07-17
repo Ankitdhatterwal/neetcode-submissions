0864015653576e618c46c00b73e674bc1f6e78d8
Ankit Dhatterwal <90125082+Ankitdhatterwal@users.noreply.github.com>
2026-07-17 09:12:43 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sArray = s.toCharArray();
+char[] tArray = t.toCharArray();
+
+Arrays.sort(sArray);
+Arrays.sort(tArray);
+
+
+return Arrays.equals(sArray,tArray);
+        // if(sArray.length != tArray.length )
+        // for(int i =0 ; i< sArray.length; i++){
+        //     boolean found = false;
+        //     for(int j = 0; j< tArray.length; j++)
+        //     {
+        //        if(sArray[i]==tArray[j]){
+        //         tArray[j] = '.';
+        //         System.out.println("found : "+ sArray[i]+"="+ tArray[j]);
+        //         found = true;
+        //         break;
+        //        }
+        //     }
+        //     if(!found) return false;
+        // }
+        // return true;
+}
+}
